7f52bd315510037376241dca5982b01e038c3f1a
Pooja9783 <poojasankhala97@gmail.com>
2025-02-26 15:24:50 +0530

    removed unnecessary package

--- backend/server.js
+++ backend/server.js
@@ -60,6 +60,6 @@ app.get("*", (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
